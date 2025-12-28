9554cd541136eb143a17106d1d199e23f9be7398
Paulo Fernandes Vieira <soeupfv@hotmail.com>
2025-12-28 14:24:10 -0300

    estrutura inicial

---
+++ src/main/java/br/com/vendas/passagem/omnibus/OmnibusApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.vendas.passagem.omnibus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmnibusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmnibusApiApplication.class, args);
+	}
+
+}
